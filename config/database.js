37cde17ff5e080be31ebf11b9971ef63144e76cd
Antoine Paturel <antoine.paturel34@free.fr>
2023-08-20 08:38:43 +0200

    BDD

--- config/database.js
+++ config/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('PGHOST', '127.0.0.1'),
       port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      database: env('PGDATABASE', 'strapi'), // changer entre strapi et dbantoine
+      user: env('PGUSER', 'strapi'), // changer entre strapi et postgres
+      password: env('PGPASSWORD', 'password'), // changer entre password et postgres
       ssl: env.bool(true),
     },
     pool: { min: 0 }
